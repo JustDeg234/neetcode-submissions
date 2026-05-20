4d4173b9b60b81234580de79065142ce9a4f25ce
Diego Davalos <160700274+JustDeg234@users.noreply.github.com>
2026-05-20 08:11:39 -0700

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.py
@@ -0,0 +1,27 @@
+#Create struct for Linked List Node
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode | 0, l2: ListNode | 0) -> ListNode | 0:
+        head = ListNode() #head stays
+        tail = head #tail moves
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            #update tail
+            tail = tail.next
+        
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+
+        return head.next #head initial is null (dummy node)
